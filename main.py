22ad02a81f8d16fa4c89c1f19a9c047cea1542ef
IvnMatz <ivanholaxderespaldo@gmail.com>
2024-09-28 14:47:55 -0600

    Pagina de usuario y de producto agregadas

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyodbc
 from flask import Flask, request, render_template, session, redirect, url_for
 
 app = Flask(__name__)
-app.secret_key = 'papuClave'
+app.secret_key = 'butImWithTheHomiesRightNow'
 
 # Definir los parámetros de conexión
 server = '(localdb)\\MainServer'  
@@ -12,8 +12,8 @@ conn_str = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={
 
 
 
-# ROUTES ------------------------------------------------------
-# Main Route
+# ROUTES ----------------------------------------------------------------------------
+# Main Route--------------------------------------------------------------------------
 @app.route("/")
 def index():
     user = {}
@@ -23,7 +23,7 @@ def index():
         user['auth'] = False
     return render_template('index.html', user=user)
 
-# Register Route
+# Register Route --------------------------------------------------------------------
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -49,12 +49,12 @@ def register():
 
             return "registro completo"
         except Exception as e:
-            return f"Error {e}"
+            return render_template("error.html", error=e)
 
         
     return render_template('register.html')
 
-# Login Route
+# Login Route -------------------------------------------------------------------
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -73,29 +73,62 @@ def login():
             cursor.close()
             conn.close()
             if returnedUser:
-                session['username'] = username
-                session['id'] = returnedUser[0][0]
+                session['username'] = returnedUser[0][1]
+                session['id'] = returnedUser[0][0] # 0=ID 1=Username 2=Password 3=Mail 4=Language 5=theme (0=light, 1=Dark)
                 session['is_authenticated'] = True
-                return 'Sesión iniciada !'
+                session['mail'] = returnedUser[0][3]
+                session['lang'] = returnedUser[0][4]
+                session['theme'] = returnedUser[0][5]
+                return redirect(url_for('index'))
             else:
                 return 'Usuario o contraseña Mal ingresados'
 
         except Exception as e:
-            return f"Error {e}"
+            return render_template("error.html", error=e)
 
     return render_template('login.html')
 
+#User Route -------------------------------------------------------------------------------
 @app.route('/user')
 def userpage():
-    return render_template("profPage.html")
+    user = session
+    return render_template("user.html", user=user)
 
+#Product Route ---------------------------------------------------------------------------------
 @app.route('/product/<id_product>')
 def product(id_product):
-    return f"Producto {id_product}"
+    product = {}
+    try:
+        conn = pyodbc.connect(conn_str)
+        print("Conexión exitosa")
+        cursor = conn.cursor()
+        cursor.execute(f"select * from products where id_product={id_product}")
+        returnedProd = cursor.fetchall()
+        #ID del array (returnedprod[0][ESTO])
+        # 0=ID 1=name 2=precio 3=descript 4=dimension 5=peso
+        cursor.execute(f"select users.username, review.* from review join users on review.id_user=users.id_user join products on review.id_product=products.id_product where products.id_product={id_product}")
+        returnedRev = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        
+        if returnedProd:
+            product['name'] = returnedProd[0][1]
+            product['precio'] = returnedProd[0][2]
+            product['desc'] = returnedProd[0][3]
+            product['dimension'] = returnedProd[0][4]
+            product['peso'] = returnedProd[0][5]
+        else:
+            return 'Este producto no existe, lol'
 
+    except Exception as e:
+        return render_template("error.html", error=e)
+    
+    return render_template("product.html", product=product, reviews=returnedRev)
+
+#logout route (Redirects to index) --------------------------------------------------------------
 @app.route('/logout')
 def logout():
     session.pop('is_athenticated', None)
     session.pop('username', None)
     session.pop('id', None)
-    return redirect(url_for('login'))
+    return redirect(url_for('index'))
