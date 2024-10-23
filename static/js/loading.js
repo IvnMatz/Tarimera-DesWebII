6fe562dff6f7a139c0678cddef96530ac12c1c3e
IvnMatz <ivanholaxderespaldo@gmail.com>
2024-10-22 22:39:31 -0600

    producto NEWDESIGN

---
+++ static/js/loading.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("content").style.display = "block";
+    document.getElementById("products").style.display = "block";
+    document.getElementById("navbar").style.display = "flex";
+    document.getElementById("contact").style.display = "block";
+  });
